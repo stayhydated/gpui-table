2140dd839d308984fd7a26dd9c39c672c2854053
stayhydated <56778463+stayhydated@users.noreply.github.com>
2025-11-26 00:57:14 -0500

    .

--- example/build.rs
+++
@@ -1,8 +0,0 @@
-pub fn main() {
-    if let Err(e) = es_fluent_build::FluentBuilder::new()
-        .mode(es_fluent_build::FluentParseMode::Conservative)
-        .build()
-    {
-        log::error!("Error building FTL files: {}", e);
-    }
-}
